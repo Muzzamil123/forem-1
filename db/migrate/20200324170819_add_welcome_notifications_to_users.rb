2fa253699b67d30c3afb28f7dd2bceb05864d5ef
Muzzamil123 <muzamil.mujahid11@gmail.com>
2020-08-17 21:44:34 +0500

    migrations updated

--- db/migrate/20200324170819_add_welcome_notifications_to_users.rb
+++ db/migrate/20200324170819_add_welcome_notifications_to_users.rb
@@ -3,6 +3,6 @@ class AddWelcomeNotificationsToUsers < ActiveRecord::Migration[5.2]
     # add_column :users, :welcome_notifications, :boolean, default: true, null: false
     add_column :users, :welcome_notifications, :boolean
     change_column_default :users, :welcome_notifications, true
-    change_column_null :users, :welcome_notifications, false
+    # change_column_null :users, :welcome_notifications, false
   end
 end
