ade10dc3e6611163b0bafa81206f84bb9032ac11
Muzzamil123 <muzamil.mujahid11@gmail.com>
2020-08-17 20:40:46 +0500

    migration updated

--- db/migrate/20200225104037_add_social_preview_template_to_tags.rb
+++ db/migrate/20200225104037_add_social_preview_template_to_tags.rb
@@ -1,4 +1,5 @@
 class AddSocialPreviewTemplateToTags < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
   def change
     add_column :tags, :social_preview_template, :string, default: "article"
   end
