aaf7d95032b83f11132a1d10d8c01d09df137182
Muzzamil123 <muzamil.mujahid11@gmail.com>
2020-08-17 21:49:00 +0500

    migrations updated

--- db/migrate/20200514212601_add_public_reactions_count_columns.rb
+++ db/migrate/20200514212601_add_public_reactions_count_columns.rb
@@ -7,14 +7,14 @@ class AddPublicReactionsCountColumns < ActiveRecord::Migration[5.2]
 
     add_column :articles, :public_reactions_count, :integer
     change_column_default :articles, :public_reactions_count, 0
-    change_column_null :articles, :public_reactions_count, false
+    # change_column_null :articles, :public_reactions_count, false
 
     add_column :articles, :previous_public_reactions_count, :integer
     change_column_default :articles, :previous_public_reactions_count, 0
-    change_column_null :articles, :previous_public_reactions_count, false
+    # change_column_null :articles, :previous_public_reactions_count, false
 
     add_column :comments, :public_reactions_count, :integer
     change_column_default :comments, :public_reactions_count, 0
-    change_column_null :comments, :public_reactions_count, false
+    # change_column_null :comments, :public_reactions_count, false
   end
 end
