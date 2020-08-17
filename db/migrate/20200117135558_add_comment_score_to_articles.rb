a4cdd1540d016f7a9ad7b8d77fd68970b3f711df
Muzzamil123 <muzamil.mujahid11@gmail.com>
2020-08-17 20:32:20 +0500

    migration updated

--- db/migrate/20200117135558_add_comment_score_to_articles.rb
+++ db/migrate/20200117135558_add_comment_score_to_articles.rb
@@ -1,5 +1,14 @@
 class AddCommentScoreToArticles < ActiveRecord::Migration[5.2]
-  def change
-    add_column :articles, :comment_score, :integer, default: 0
+  # def change
+  #   add_column :articles, :comment_score, :integer, default: 0
+  # end
+  def up
+    add_column :articles, :comment_score, :integer
+    change_column_default :articles, :comment_score, 0
+  end
+
+  def down
+    # remove_column :users, :some_column
+    safety_assured { remove_column :articles, :comment_score }
   end
 end
