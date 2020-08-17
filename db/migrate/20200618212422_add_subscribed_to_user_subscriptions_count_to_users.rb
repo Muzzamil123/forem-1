7f064796e10a11a66132dd5a353fabd4871a6a89
Muzzamil123 <muzamil.mujahid11@gmail.com>
2020-08-17 21:32:23 +0500

    migrations updated

--- db/migrate/20200618212422_add_subscribed_to_user_subscriptions_count_to_users.rb
+++ db/migrate/20200618212422_add_subscribed_to_user_subscriptions_count_to_users.rb
@@ -1,9 +1,12 @@
 class AddSubscribedToUserSubscriptionsCountToUsers < ActiveRecord::Migration[6.0]
   def self.up
-    add_column :users, :subscribed_to_user_subscriptions_count, :integer, null: false, default: 0
+    # add_column :users, :subscribed_to_user_subscriptions_count, :integer, null: false, default: 0
+    add_column :users, :subscribed_to_user_subscriptions_count, :integer
+    change_column_default :users, :subscribed_to_user_subscriptions_count, 0
+    change_column_null :users, :subscribed_to_user_subscriptions_count, false
   end
 
   def self.down
-    remove_column :users, :subscribed_to_user_subscriptions_count
+    safety_assured { remove_column :users, :subscribed_to_user_subscriptions_count }
   end
 end
--- db/migrate/20200706184804_add_user_subscriptions_count_to_articles.rb
+++ db/migrate/20200706184804_add_user_subscriptions_count_to_articles.rb
@@ -1,9 +1,12 @@
 class AddUserSubscriptionsCountToArticles < ActiveRecord::Migration[6.0]
   def self.up
-    add_column :articles, :user_subscriptions_count, :integer, null: false, default: 0
+    # add_column :articles, :user_subscriptions_count, :integer, null: false, default: 0
+    add_column :articles, :user_subscriptions_count, :integer
+    change_column_default :articles, :user_subscriptions_count, 0
+    change_column_null :articles, :user_subscriptions_count, false
   end
 
   def self.down
-    remove_column :articles, :user_subscriptions_count
+    safety_assured { remove_column :articles, :user_subscriptions_count }
   end
 end
